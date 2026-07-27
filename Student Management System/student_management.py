c9015668a00ccd5c2fe4b14c952a12bfb2406a1d
Hamza <hamzasaeed.bwp1@gmail.com>
2026-07-27 12:22:48 +0500

    Completed Student Management System with menu

--- Student Management System/student_management.py
+++ Student Management System/student_management.py
@@ -91,19 +91,66 @@ class Student_management():
 
 system = Student_management()
 
-s1 = Student("1001", "Junaid", 21, "Computer Science", [78,98,67])
-s2 = Student("1002", "Raheem", 20, "Software Science", [77,94,68])
+# s1 = Student("1001", "Junaid", 21, "Computer Science", [78,98,67])
+# s2 = Student("1002", "Raheem", 20, "Software Science", [77,94,68])
 
-system.add_students(s1)
-system.add_students(s2)
+# system.add_students(s1)
+# system.add_students(s2)
+# system.display_all_students()
+# system.search_student("1003")
+# system.search_student("1002",)
+# system.update_marks("1001", [89,78,87])
+# system.delete_student("1003")
+# system.search_student_name("Raheem",)
+# print(s1.calculate_average())
+# print(s1.calculate_grade())
+# system.top_student()
 
-system.display_all_students()
+while True:
+    print("1. Add Student")
+    print("2. Display All Students")
+    print("3. Search Student by ID")
+    print("4. Search Student by Name")
+    print("5. Update Marks")
+    print("6. Delete Student")
+    print("7. Top Student")
+    print("8. Exit")
 
-system.search_student("1003")
-system.search_student("1002",)
-system.update_marks("1001", [89,78,87])
-system.delete_student("1003")
-system.search_student_name("Raheem",)
-print(s1.calculate_average())
-print(s1.calculate_grade())
-system.top_student()
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        student_id = input("Enter Student ID: ")
+        name = input("Enter Name: ")
+        age = int(input("Enter Age: "))
+        course = input("Enter Course: ")
+        marks = list(map(int, input("Enter marks separated by spaces: ").split()))
+
+        student = Student(student_id, name, age, course, marks)
+        system.add_students(student)
+        print("Student added successfully!")
+
+    elif choice == "2":
+        system.display_all_students()
+    elif choice == "3":
+        student_id = input("Enter Student ID: ")
+        system.search_student(student_id)
+    elif choice == "4":
+        name = input("Enter Student Name: ")
+        system.search_student_name(name)
+    elif choice == "5":
+        student_id = input("Enter Student ID: ")
+        new_marks = list(map(int, input("Enter new marks separated by spaces: ").split()))
+        system.update_marks(student_id, new_marks)
+    elif choice == "6":
+        student_id = input("Enter Student ID: ")
+        system.delete_student(student_id)
+    elif choice == "7":
+        system.top_student()
+    elif choice == "8":
+        print("Goodbye!")
+        break
+    else:
+         print("Invalid choice")
+         
+        
+
+                                    
